a6fba070dc38f0edeb789815bb5e88210b66a979
lindabettini <linda.bettini@hotmail.it>
2022-03-04 15:50:10 +0100

    change construct

--- main_components/Carrello.php
+++ main_components/Carrello.php
@@ -5,14 +5,16 @@ class Carrello
 {
   public $prodotti_selezionati = [];
 
+
   public function __construct(array $prodotti_selezionati,)
   {
-    $this->aggiungiAlCarrello($prodotto) = $prodotti_selezionati;
+    $this->aggiungiAlCarrello($prodotto);
   }
+
 
   public function aggiungiAlCarrello($prodotto)
   {
     if (!($prodotto instanceof Articolo)) throw new Exception('Articolo non trovato');
-    $this->$prodotti_selezionati = $prodotto;
+    $this->prodotti_selezionati = $prodotto;
   }
 }
